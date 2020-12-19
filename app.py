4324dcce248d45531ae3ece9c1a9c17618e55de5
johan456789 <14802181+johan456789@users.noreply.github.com>
2020-12-20 02:38:44 +0800

    initial commit

---
+++ app.py
@@ -0,0 +1,49 @@
+from plistlib import readPlist
+from AppKit import NSWorkspace
+from sys import stdout
+from sys import argv
+from xml.parsers.expat import ExpatError
+
+
+def binary_plist_to_file_obj(filename):
+    "Pipe the binary plist through plutil and return as file object"
+    from subprocess import Popen, PIPE
+    from StringIO import StringIO
+
+    with open(filename, "rb") as f:
+        content = f.read()
+    args = ["plutil", "-convert", "xml1", "-o", "-", "--", "-"]
+    p = Popen(args, stdin=PIPE, stdout=PIPE)
+    out, err = p.communicate(content)
+    return StringIO(out)
+
+
+path = ''
+
+if len(argv) > 1:
+    path = argv[1]
+
+if path == '':
+    path = NSWorkspace.sharedWorkspace().activeApplication().get('NSApplicationPath')
+
+path += '/Contents/Info.plist'
+try:
+    info = readPlist(path)
+except(ExpatError):  # binary plist
+    info = readPlist(binary_plist_to_file_obj(path))
+
+appName = info.get('CFBundleExecutable')
+if 'CFBundleDisplayName' in info:
+    appName = info.get('CFBundleDisplayName')
+elif 'CFBundleName' in info:
+    appName = info.get('CFBundleName')
+
+appVersion = info.get('CFBundleVersion')
+appShortVersion = info.get('CFBundleShortVersionString')
+if appShortVersion and appVersion:
+    if appVersion != appShortVersion:
+        appVersion = appShortVersion + ' (' + appVersion + ')'
+elif not appVersion and appShortVersion:
+    appVersion = appShortVersion
+
+stdout.write(appName + ' v' + appVersion)
